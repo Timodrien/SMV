fa7d22fae19057810d78c473f95fd9267f537dd7
Timodrien <weide.timo@outlook.com>
2025-01-21 16:41:07 +0100

    Update Movements.cpp

--- SMV_code/Movements.cpp
+++ SMV_code/Movements.cpp
@@ -24,84 +24,84 @@ void initMotor() {
 void Vor(){
 //Rechte Seite
   //Motor VL
-  pcf8574.digitalWrite(0, LOW);  // Motor 1 beginnt zu rotieren
+  pcf8574.digitalWrite(0, LOW);  // Motor 1
   pcf8574.digitalWrite(1, HIGH);
   //Motor HL
-  pcf8574.digitalWrite(2, LOW);  // Motor 2 beginnt zu rotieren
+  pcf8574.digitalWrite(2, LOW);  // Motor 2
   pcf8574.digitalWrite(3, HIGH);
 //Linke Seite
   //Motor VR
-  pcf8574.digitalWrite(4, HIGH);  // Motor 3 beginnt zu rotieren
+  pcf8574.digitalWrite(4, HIGH);  // Motor 3
   pcf8574.digitalWrite(5, LOW);
   //Motor HR
-  pcf8574.digitalWrite(6, HIGH);  // Motor 4 beginnt zu rotieren
+  pcf8574.digitalWrite(6, HIGH);  // Motor 4
   pcf8574.digitalWrite(7, LOW);
 }
 
 void Rueck(){
 //Rechte Seite
   //Motor VL
-  pcf8574.digitalWrite(0, HIGH);  // Motor 1 beginnt zu rotieren
+  pcf8574.digitalWrite(0, HIGH);  // Motor 1
   pcf8574.digitalWrite(1, LOW);
   //Motor HL
-  pcf8574.digitalWrite(2, HIGH);  // Motor 2 beginnt zu rotieren
+  pcf8574.digitalWrite(2, HIGH);  // Motor 2
   pcf8574.digitalWrite(3, LOW);
 //Linke Seite
   //Motor VR
-  pcf8574.digitalWrite(4, LOW);  // Motor 3 beginnt zu rotieren
+  pcf8574.digitalWrite(4, LOW);  // Motor 3
   pcf8574.digitalWrite(5, HIGH);
   //Motor HR
-  pcf8574.digitalWrite(6, LOW);  // Motor 4 beginnt zu rotieren
+  pcf8574.digitalWrite(6, LOW);  // Motor 4
   pcf8574.digitalWrite(7, HIGH);
 }
 
 void Links(){
 //Rechte Seite
   //Motor VL
-  pcf8574.digitalWrite(0, LOW);  // Motor 1 beginnt zu rotieren
+  pcf8574.digitalWrite(0, LOW);  // Motor 1
   pcf8574.digitalWrite(1, HIGH);
   //Motor HL
-  pcf8574.digitalWrite(2, LOW);  // Motor 2 beginnt zu rotieren
+  pcf8574.digitalWrite(2, LOW);  // Motor 2
   pcf8574.digitalWrite(3, HIGH);
 //Linke Seite
   //Motor VR
-  pcf8574.digitalWrite(4, LOW);  // Motor 3 beginnt zu rotieren
+  pcf8574.digitalWrite(4, LOW);  // Motor 3
   pcf8574.digitalWrite(5, HIGH);
   //Motor HR
-  pcf8574.digitalWrite(6, LOW);  // Motor 4 beginnt zu rotieren
+  pcf8574.digitalWrite(6, LOW);  // Motor 4
   pcf8574.digitalWrite(7, HIGH);
 }
 
 void Rechts(){
 //Rechte Seite
   //Motor VL
-  pcf8574.digitalWrite(0, HIGH);  // Motor 1 beginnt zu rotieren
+  pcf8574.digitalWrite(0, HIGH);  // Motor 1
   pcf8574.digitalWrite(1, LOW);
   //Motor HL
-  pcf8574.digitalWrite(2, HIGH);  // Motor 2 beginnt zu rotieren
+  pcf8574.digitalWrite(2, HIGH);  // Motor 2
   pcf8574.digitalWrite(3, LOW);
 //Linke Seite
   //Motor VR
-  pcf8574.digitalWrite(4, HIGH);  // Motor 3 beginnt zu rotieren
+  pcf8574.digitalWrite(4, HIGH);  // Motor 3
   pcf8574.digitalWrite(5, LOW);
   //Motor HR
-  pcf8574.digitalWrite(6, HIGH);  // Motor 4 beginnt zu rotieren
+  pcf8574.digitalWrite(6, HIGH);  // Motor 4
   pcf8574.digitalWrite(7, LOW);
 }
 
 void Stop(){
 //Rechte Seite
   //Motor VL
-  pcf8574.digitalWrite(0, LOW);  // Motor 1 beginnt zu rotieren
+  pcf8574.digitalWrite(0, LOW);  // Motor 1
   pcf8574.digitalWrite(1, LOW);
   //Motor HL
-  pcf8574.digitalWrite(2, LOW);  // Motor 2 beginnt zu rotieren
+  pcf8574.digitalWrite(2, LOW);  // Motor 2
   pcf8574.digitalWrite(3, LOW);
 //Linke Seite
   //Motor VR
-  pcf8574.digitalWrite(4, LOW);  // Motor 3 beginnt zu rotieren
+  pcf8574.digitalWrite(4, LOW);  // Motor 3
   pcf8574.digitalWrite(5, LOW);
   //Motor HR
-  pcf8574.digitalWrite(6, LOW);  // Motor 4 beginnt zu rotieren
+  pcf8574.digitalWrite(6, LOW);  // Motor 4
   pcf8574.digitalWrite(7, LOW);
 }
